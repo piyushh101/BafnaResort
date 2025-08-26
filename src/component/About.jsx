a65f901c946c8614103a97515ce2f52655ebd43f
piyushh101 <158832228+piyushh101@users.noreply.github.com>
2025-08-26 13:23:05 +0530

    Update About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./About.css";
 import Typewriter from "typewriter-effect";
 
-// swap these with your real images in /src/ASSETS
 import imgSmall1 from "../ASSETS/LEFT.jpg";
 import imgSmall2 from "../ASSETS/LEFTONE.jpg";
 import imgTall   from "../ASSETS/RIGHT.jpg";
@@ -18,34 +17,38 @@ export default function About() {
             Welcome To <br />
             <span>Roberto Hotel Luxury</span>
           </h2>
+
           <div className="typewriter-text">
-       <Typewriter
-  options={{
-    strings: [
-      "With over 340 hotels worldwide, NH Hotel Group offers a wide variety of hotels.",
-      "Catering for a perfect stay no matter where your destination."
-    ],
-    autoStart: true,
-    loop: true,
-    delay: 40,
-  }}
-/>
-</div>
+            <Typewriter
+              options={{
+                strings: [
+                  "With over 340 hotels worldwide, NH Hotel Group offers a wide variety of hotels.",
+                  "Catering for a perfect stay no matter where your destination."
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 40
+              }}
+            />
+          </div>
 
           <div className="about__meta">
             <p><strong>Manager:</strong> Micheal Jordan</p>
-            {/* remove signature as requested */}
           </div>
         </div>
 
-        {/* Right: Image Grid */}
+        {/* Right: Image Grid (3 items; tall spans 2 rows) */}
         <div className="about__grid">
-          <div className="about__gridLeft">
+          <div className="about__imgWrap">
             <img src={imgSmall1} alt="pool terrace" className="about__img about__img--card" />
-            <img src={imgSmall2} alt="infinity pool" className="about__img about__img--card" />
           </div>
-          <div className="about__gridRight">
+
+          <div className="about__imgWrap about__imgWrap--tall">
             <img src={imgTall} alt="sea kayaking" className="about__img about__img--tall" />
+          </div>
+
+          <div className="about__imgWrap">
+            <img src={imgSmall2} alt="infinity pool" className="about__img about__img--card" />
           </div>
         </div>
       </div>
